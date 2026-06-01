11464f98e53905456e653e5cddfec54e974fa155
dipucoderop <dipjyotib357@gmail.com>
2026-06-02 01:09:51 +0530

    INPUT FIXED

--- game.py
+++ game.py
@@ -2,6 +2,7 @@ import random
 C=str(input("ENTER YOUR USERNAME SIR:"))
 print("WELCOME",C)
 h=str(input("TYPE LAL SALAM TO START THE GAME:"))
+M=0
 if h.upper()=="LAL SALAM":
     print("OK! LETS START THE GAME")
     print("YOU HAVE TO SCORE 69 TO WIN THE GAME")
@@ -9,19 +10,29 @@ if h.upper()=="LAL SALAM":
     b=0
     x=69/v
     print("you will be awarded", x ,"each time you win")
-    print("YOU HAVE TYPE ROCK,PAPER,SCISSOR OR ROPE")
+    print("YOU HAVE TO TYPE ROCK,PAPER,SCISSOR OR ROPE AND IF YOU GIVE INVALID INPUT MORE THAN ONE TIME YOU WILL BE DISQUALIFIED")
     print()
     for i in range(v):
+        n=["ROCK","PAPER","SCISSOR","ROPE"]
         d=random.choice(["ROCK","PAPER","SCISSOR","ROPE"])
         e=str(input("YOU:"))
-        print()
-        print("COMPUTER:",d)
-        if e.upper() == d:
-            print("CONGRATS! YOU WON THIS ROUND")
-            b+=x
+        if e.upper() in n:
+           print()
+           if e.upper() == d:
+               print("CONGRATS! YOU WON THIS ROUND")
+               b+=x
+           else:
+                print("Regrets you lost this round")
+           print()
         else:
-            print("Regrets you lost this round")
-        print()
+            print("DEAR",C,"PLEASE GIVE VALID INPUT AND IF YOU AGAIN GIVE WRONG INPUT YOU WILL BE DISQUALIFIED")
+            M+=1
+            print( )
+            if M>1:
+              print("YOU ARE DISQUALIFIED")
+              break
+            else:
+                continue
     if b>=69:
         print("YOU WON ")
     else:
